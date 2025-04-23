73bf12f9f710f7b3ed701046df6f31c505d26218
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2025-04-23 19:16:12 +0530

    Merged repo-b into tutorial/javascript-learning

---
+++ javascript-learning/builtin-obj2.js
@@ -0,0 +1,6 @@
+/* json is a build in object in java in json not support function in its object json syntax is not support single quotes,json sytax is universal 
+
+*/
+let obj = {name:"xxx",Age:25,Gender:'y','class-school':'XII',bool:{tree:"Mango",fruit:"Apple"},func: function fun(){console.log("I am inside");}}; //here name is property and "XXX" is a value
+console.log(localStorage.getItem('a'));
+localStorage.setItem('a','Hii');
